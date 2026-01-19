cb58969e78b6023e0148249aa70eb84832034ed5
dpk <47978564+gh0st-007@users.noreply.github.com>
2026-01-19 16:08:27 +0530

    Create Mux4to1.v

---
+++ src/Mux4to1.v
@@ -0,0 +1,11 @@
+module Mux4to1(
+        input [31:0] in1,in2,in3,in4,
+        input [1:0] sel,
+        output [31:0] out
+    );
+    
+    assign out = (sel==2'b00) ? in1 : 
+                 (sel == 2'b01) ? in2 :
+                 (sel == 2'b10) ? in3 : in4;
+    
+endmodule
